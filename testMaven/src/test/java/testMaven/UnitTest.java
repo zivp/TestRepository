ab40514906a4f70330681b8482f59bb22a48c70a
zivp <zivp1010@gmail.com>
2020-02-01 18:51:39 +0200

    upload projact

---
+++ testMaven/src/test/java/testMaven/UnitTest.java
@@ -0,0 +1,17 @@
+package testMaven;
+
+import org.testng.annotations.Test;
+
+
+public class UnitTest {
+
+
+		
+		  @Test
+		  public void Test1() {
+			  
+				System.out.println("hello world");
+				
+		  }
+
+}
