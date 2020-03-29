ef08ec2931dd2a86f502886bee2509382bcd7018
zivp <zivp1010@gmail.com>
2020-03-29 19:23:47 +0300

    add

--- testMaven/src/test/java/testMaven/UnitTest.java
+++ testMaven/src/test/java/testMaven/UnitTest.java
@@ -1,18 +1,36 @@
 package testMaven;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.BeforeClass;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
+
+import pages.BasePage;
 
 
 
 public class UnitTest {
-
+	
+	WebDriver driver;
 
 		
 		  @Test
-		  public void Test1() {
-			  
-				System.out.println("hello world");
-				
+		  public void Test1() throws Exception {
+			  try{
+			driver= BasePage.initChromeDriver("https://www.google.com");
+			  BasePage.getElement("//div[3]/center/input[1]", "xpath");
+			  }
+			  catch(Exception e) {
+				  e.getStackTrace();
+				 BasePage.takeSnapShot(driver);
+			  }
 		  }
+		  
+		  
+		  
 
 }
